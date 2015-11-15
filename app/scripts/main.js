2efacd6c3238356f8f38748a36f047b596b56d02
Kyle McNally <kyle@kmcnally.net>
2015-11-14 21:18:05 -0500

    Initial experience done
    Added UX for switching payment types

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,32 @@
 console.log('\'Allo \'Allo!');
+
+function updatePaymentType(paymentType){
+    //This function takes a payment type and unhides the correct div
+    if(paymentType === 'credit'){
+      console.log('Credit');
+      document.getElementById('credit-card').style.display = 'block';
+      document.getElementById('ach').style.display = 'none';
+    } else if(paymentType === 'ach'){
+      console.log('ACH');
+      document.getElementById('credit-card').style.display = 'none';
+      document.getElementById('ach').style.display = 'block';
+    } else {
+      console.log('Bad Payment type');
+      document.getElementById('credit-card').style.display = 'none';
+      document.getElementById('ach').style.display = 'none';
+    }
+}
+
+//Run it on document load
+//Find the value of Payment type radio
+var formElements = document.getElementById('payment-form').elements;
+var radios = formElements['payment-type'];
+
+updatePaymentType(radios.value);
+
+//Then bind it to the radio buttons
+for(var i = 0; i < radios.length; i++){
+  radios[i].onclick = function(){
+    updatePaymentType(this.value);
+  }
+}
