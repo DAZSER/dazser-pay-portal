6023bb65fa8f08b5c7c8beb0b70621c54580b95a
Kyle McNally <kyle@kmcnally.net>
2015-12-04 14:41:06 -0500

    Implement .remove() for Elements

--- app/scripts/ux.js
+++ app/scripts/ux.js
@@ -44,3 +44,15 @@ amountInput.addEventListener('change', function(){
   document.getElementById('span-amount').textContent = newAmount;
   document.getElementById('total-amount').value = newAmount;
 });
+
+//Augment the DOM to add remove functions!
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
