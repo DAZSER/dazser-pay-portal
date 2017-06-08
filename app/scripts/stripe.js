1e374b33edcbd5ccde0b4dfcd4a2932d1a6b4b28
Kyle McNally <kyle@kmcnally.net>
2017-06-08 12:33:43 -0400

    Upgrade to Stripe.js Version 3

--- app/scripts/stripe.js
+++ app/scripts/stripe.js
@@ -1,6 +1,53 @@
 'use strict';
 
-//https://stripe.com/docs/tutorials/forms
+//https://stripe.com/docs/elements
+
+//Load my Stripe Key
+var stripe = Stripe('pk_live_j6A6g7uGYYkLAwXzs1CKA8bH');
+//var stripe = Stripe('pk_test_cZSmFhp4VdaBYUOCq0eOsS1r');
+var elements = stripe.elements();
+
+var card = elements.create('card');
+
+card.mount('#card-element');
+
+function createToken(){
+  stripe.createToken(card).then(function(result){
+    if (result.error) {
+      //Inform the user of the error
+      var errorElement = document.getElementById('error-holder');
+      errorElement.textContent = result.error.message;
+      errorElement.display = 'block';
+      payButtonStateChanger('submittable');
+    } else {
+      // Attach the token to the form and send it to my server
+      stripeResponseCreditHandler(result.token);
+    }
+  });
+}
+
+//This function is the Credit card response handler
+//createToken sends the CC info and returns a promise, this function is
+//called when the promise is fulfilled
+function stripeResponseCreditHandler(token){
+  //Add the token, then submit to my server
+  //I will process the token with Stripe in order to charge the customer
+  var form = document.getElementById('payment-form');
+  //Create the element
+  var tokenInput = document.createElement('input');
+  tokenInput.type = 'hidden';
+  tokenInput.name = 'stripeToken';
+  tokenInput.value = JSON.stringify(token);
+  //And append it to the form!
+  form.appendChild(tokenInput);
+
+  //Add the action for the form
+  form.action = 'process.php';
+  form.method = 'POST';
+
+  //Finally, submit the form!
+  form.submit();
+}
 
 //Grab a handler for the pay button globally
 var payButton = document.getElementById('pay-button');
@@ -32,69 +79,12 @@ function payButtonStateChanger(state){
   }
 }
 
-//This function is the Credit card response handler
-//.createToken sends the form, along with a promise, the function is
-//called when the promise is fulfilled
-function stripeResponseCreditHandler(status, response){
-  var form = document.getElementById('payment-form');
-
-  if(response.error) {
-    // Show the errors to the user
-    console.log(response);
-    var errorElement = document.getElementById('error-holder');
-    errorElement.textContent = response.error.message;
-    errorElement.display = 'block';
-    payButtonStateChanger('submittable');
-  } else {
-    //NO error!!! Add the token, then submit to my server
-    //I will process the token with Stripe in order to charge the customer
-    console.log(response);
-    //Create the element
-    var token = document.createElement('input');
-    token.type = 'hidden';
-    token.name = 'stripeToken';
-    token.value = response.id;
-    //And append it to the form!
-    form.appendChild(token);
-    //Also attach the entire response object!
-    var stripeResponse = document.createElement('input');
-    stripeResponse.type = 'hidden';
-    stripeResponse.name = 'stripeResponse';
-    stripeResponse.value = JSON.stringify(response);
-    form.appendChild(stripeResponse);
-
-    //Add the action for the form
-    form.action = 'process.php';
-    form.method = 'POST';
-
-    //Finally, submit the form!
-    console.log('Submit!');
-    form.submit();
-  }
-}
-
 //Create the single use token by sending the CC information to Stripe
 //This will return a token that I can store on my DB server.
 //I can then use that token to charge the customer
-payButton.addEventListener('click', function(event){
-  console.log('Cancel form submit quickly');
+payButton.addEventListener('click', (event)=>{
   event.preventDefault();
-});
-payButton.addEventListener('click', function(){
-  console.log('Pay button clicked');
-
   //Change the state of the pay button
   payButtonStateChanger('submitting');
-
-  //Get the value of the Radio button
-  var form = document.getElementById('payment-form');
-  //var radios = form.elements['payment-type'];
-  //if(radios.value === 'credit'){
-    //Run Stripe code for Credit Cards
-    Stripe.card.createToken(form, stripeResponseCreditHandler);
-  //} else if (radios.value === 'ach') {
-    //Run Stripe code for ACH
-  //} else {
-    //Invalid Radio value
-  //}
+  createToken();
 });
